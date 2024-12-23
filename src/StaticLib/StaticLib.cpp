cad1b8caf198ebfdea470c13ee6045fb22ce7a7d
kanisaur <seto3ka4u@outlook.jp>
2024-12-23 13:07:02 +0900

    書き換えました

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -6,5 +6,24 @@
 // バブルソート(引数が不適切であればfalseを返す)
 bool bubble_sort(item* begin, const item* end)
 {
-	return false;
+    if (begin == nullptr || end == nullptr || begin >= end) 
+    {
+        return false;
+    }
+
+    size_t n = end - begin;
+    for (size_t i = 0; i < n - 1; ++i)
+    {
+        for (size_t j = 0; i < n - 1; j++)
+        {
+            if (begin[j].key > begin[j + 1].key)
+            {
+                item temp = begin[j];
+                begin[j] = begin[j + 1];
+                begin[j + 1] = temp;
+            }
+        }
+    }
+	
+	return true;
 }
